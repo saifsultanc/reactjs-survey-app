a4e20218a547964fdce4194f5346ba3939649319
Saif <saifraikkonen@gmail.com>
2020-01-01 11:43:10 +0530

    updated json store with XHR

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
   const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let data = await axios("https://api.myjson.com/bins/vhnj4");
+  // old code with axios and myjson
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     let data = await axios("https://api.myjson.com/bins/vhnj4");
 
-      setQuestions(data["data"]["questions"]);
+  //     setQuestions(data["data"]["questions"]);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // updated implementation with jsonbin and XHR
+  useEffect(() => {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState === XMLHttpRequest.DONE) {
+        setQuestions(JSON.parse(req.responseText)["questions"]);
+      }
     };
-    fetchData();
+
+    req.open("GET", "https://api.jsonbin.io/b/5e09f7fa32536c77d6787c2d", true);
+    req.setRequestHeader(
+      "secret-key",
+      "$2b$10$wJ0vzO4WCWzkmrBmRHpemeUADwt74vhLkrxYyoSITR3gGRmsh074W"
+    );
+    req.send();
   }, []);
 
   return <Form title="This is dummy text" questions={questions} />;
